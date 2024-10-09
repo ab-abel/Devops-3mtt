c2cd8316fe54c1eb2a81df960bca5cd8999a5191
ab-abel <ab.bell205@gmail.com>
2024-10-09 21:28:57 +0100

    testing profile link

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders profile link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Dashboard/i);
+  const linkElement = screen.getByText(/Profile /i);
   expect(linkElement).toBeInTheDocument();
 });
