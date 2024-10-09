218cad3c61cb9fd6d1b576f76c77855efab18652
ab-abel <ab.bell205@gmail.com>
2024-10-09 13:47:35 +0100

    added student welcome page

---
+++ src/components/StudentWelcome.js
@@ -0,0 +1,15 @@
+// src/components/StudentWelcome.js
+import React from 'react';
+
+const StudentWelcome = () => {
+    return (
+        <div className="student-welcome">
+            <h2>Hello Abel, Welcome to your 3MTT Dashboard</h2>
+            <div className="student-image">
+                <img src="student.png" alt="Student" />
+            </div>
+        </div>
+    );
+};
+
+export default StudentWelcome;
