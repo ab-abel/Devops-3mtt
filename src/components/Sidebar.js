309ec80c167d7e92a011673a5c2a18f9d0098ba2
ab-abel <ab.bell205@gmail.com>
2024-10-09 18:45:02 +0100

    add href links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,14 +8,14 @@ const Sidebar = () => {
             <img src={logo} alt="Logo" className="logo" />
             <button><i className="fas fa-qrcode"></i>Dashboard</button>
             <ul className="sidebar-links">
-                <li><i className="fas fa-user"></i><a href="#">Profile</a></li>
-                <li><i className="fas fa-users"></i><a href="#">Community</a></li>
-                <li><i className="fas fa-book"></i><a href="#">My Courses</a></li>
-                <li><i className="fas fa-calendar-check"></i><a href="#">ALC Attendance</a></li>
-                <li><i className="fas fa-phone"></i><a href="#">Jobs & Gigs</a></li>
-                <li><i className="fas fa-clipboard-list"></i><a href="#">Surveys</a></li>
-                <li><i className="fas fa-certificate"></i><a href="#">Certificates</a></li>
-                <li><i className="fas fa-question-circle"></i><a href="#">FAQs</a></li>
+                <li><i className="fas fa-user"></i><a href="https://github.com/ab-abel/Devops-3mtt">Profile</a></li>
+                <li><i className="fas fa-users"></i><a href="https://github.com/ab-abel/Devops-3mtt">Community</a></li>
+                <li><i className="fas fa-book"></i><a href="https://github.com/ab-abel/Devops-3mtt">My Courses</a></li>
+                <li><i className="fas fa-calendar-check"></i><a href="https://github.com/ab-abel/Devops-3mtt">ALC Attendance</a></li>
+                <li><i className="fas fa-phone"></i><a href="https://github.com/ab-abel/Devops-3mtt">Jobs & Gigs</a></li>
+                <li><i className="fas fa-clipboard-list"></i><a href="https://github.com/ab-abel/Devops-3mtt">Surveys</a></li>
+                <li><i className="fas fa-certificate"></i><a href="https://github.com/ab-abel/Devops-3mtt">Certificates</a></li>
+                <li><i className="fas fa-question-circle"></i><a href="https://github.com/ab-abel/Devops-3mtt">FAQs</a></li>
             </ul>
         </div>
     );
--- src/components/StudentInfo.js
+++ src/components/StudentInfo.js
@@ -4,7 +4,7 @@ import React from 'react';
 const StudentInfo = () => {
     return (
         <div className="student-info">
-            <p><i className="fas fa-exclamation-circle"></i> See list of all selected fellows for cohort 2 <a href="#">here</a></p>
+            <p><i className="fas fa-exclamation-circle"></i> See list of all selected fellows for cohort 2 <a href="https://github.com/ab-abel/Devops-3mtt">here</a></p>
         </div>
     );
 };
