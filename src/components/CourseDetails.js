8b98a605646a0dda0b373c9373f4a26063e6157f
ab-abel <ab.bell205@gmail.com>
2024-10-09 14:45:07 +0100

    included more courses

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -1,5 +1,6 @@
 // src/components/CourseDetails.js
 import React from 'react';
+import play from '../images/play.png'; 
 
 const CourseDetails = () => {
     return (
@@ -7,11 +8,24 @@ const CourseDetails = () => {
             <div className="course">
                 <div>
                     <h2>Accessing your 3MTT dashboard</h2>
-                    <img src="play.png" alt="video" />
+                    <img src={play} alt="video" />
                     <p>In this video, you will be guided through the process of accessing your 3MTT dashboard using your email and password.</p>
                 </div>
             </div>
-            {/* Repeat for other courses */}
+            <div className="course">
+                <div>
+                    <h2>Accessing your 3MTT dashboard</h2>
+                    <img src={play} alt="video" />
+                    <p>In this video, you will be guided through the process of accessing your 3MTT dashboard using your email and password.</p>
+                </div>
+            </div>
+            <div className="course">
+                <div>
+                    <h2>Accessing your 3MTT dashboard</h2>
+                    <img src={play} alt="video" />
+                    <p>In this video, you will be guided through the process of accessing your 3MTT dashboard using your email and password.</p>
+                </div>
+            </div>
         </div>
     );
 };
