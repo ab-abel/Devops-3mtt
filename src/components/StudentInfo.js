f8cc6f26eb952e4d92a52b25ae1a7f7ac413655c
ab-abel <ab.bell205@gmail.com>
2024-10-09 13:47:25 +0100

    added student info session page

---
+++ src/components/StudentInfo.js
@@ -0,0 +1,12 @@
+// src/components/StudentInfo.js
+import React from 'react';
+
+const StudentInfo = () => {
+    return (
+        <div className="student-info">
+            <p><i className="fas fa-exclamation-circle"></i> See list of all selected fellows for cohort 2 <a href="#">here</a></p>
+        </div>
+    );
+};
+
+export default StudentInfo;
