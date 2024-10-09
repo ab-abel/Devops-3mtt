b2611ffbd51fb330e71cc70275d3dd705edd7330
ab-abel <ab.bell205@gmail.com>
2024-10-09 13:45:47 +0100

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+// src/components/Header.js
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header-image">
+            <div className="header">
+                <i className="fas fa-bars menu-icon"></i>
+                <h1>Dashboard</h1>
+            </div>
+            <hr />
+        </div>
+    );
+};
+
+export default Header;
