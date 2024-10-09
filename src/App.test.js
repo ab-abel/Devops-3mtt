9ac97e098ae7f29a4b6b0b0f6c5e218d6164a67a
ab-abel <ab.bell205@gmail.com>
2024-10-09 23:53:40 +0100

    added target test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Sidebar from './components/Sidebar';
 
 test('renders profile link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/profile /i);
+  render(<Sidebar />);
+
+  const linkElement = screen.getByText(/profile/i);
   expect(linkElement).toBeInTheDocument();
 
   expect(linkElement).toHaveAttribute('href', 'https://github.com/ab-abel/Devops-3mtt');
+
+
+  // Check if the link opens in a new tab
+  expect(linkElement).toHaveAttribute('target', '_blank');
+  expect(linkElement).toHaveAttribute('rel', 'noopener noreferrer');
 });
