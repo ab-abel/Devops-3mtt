635c59c6b1d794269ebcab22e3ea3ca049cbcdcc
ab-abel <ab.bell205@gmail.com>
2024-11-21 20:46:00 +0100

    intial terraform file

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "google" {
+    credentials = File("gcp-key.json")
+    project     = "var.project_id"
+    region      = "var.region"
+    zone        = "var.zone"
+}
+
+resource "google_compute_instance" "devops_server" {
+    name    = "devops-server"
+    machine_type = "e2-medium"
+    zone    = "var.zone"
+
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-11"
+        }
+    }
+
+    network_interface {
+        network = "default"
+
+        access_config {
+            // assign ip adddress
+        }
+    }
+    tags = ["http-server", "https-server"]
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "allow-http-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  target_tags = ["http-server", "https-server"]
+}
