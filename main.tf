cd0b2da8c1ed09cd34cc7e3d15394b706c5558eb
ab-abel <ab.bell205@gmail.com>
2024-11-22 00:40:24 +0100

    using AWS ec2

--- main.tf
+++ main.tf
@@ -1,39 +1,49 @@
-provider "google" {
-    credentials = file("gcp-key.json")
-    project     = "var.project_id"
-    region      = "var.region"
-    zone        = "var.zone"
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "google_compute_instance" "devops_server" {
-    name    = "devops-server"
-    machine_type = "e2-medium"
-    zone    = "var.zone"
-
-    boot_disk {
-        initialize_params {
-            image = "debian-cloud/debian-11"
-        }
-    }
-
-    network_interface {
-        network = "default"
-
-        access_config {
-            // assign ip adddress
-        }
-    }
-    tags = ["http-server", "https-server"]
-}
-
-resource "google_compute_firewall" "default" {
-  name    = "allow-http-https"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443"]
+resource "aws_instance" "default" {
+  ami           = "ami-012967cc5a8c9f891"
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  tags = {
+    Name = "ExampleInstance"
   }
 
-  target_tags = ["http-server", "https-server"]
+  # Optionally, you can use user_data to configure your instance on boot
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+}
+
+resource "aws_security_group" "default" {
+  name        = "allow_http_https"
+  description = "Allow inbound HTTP and HTTPS traffic"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "instance_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.default.public_ip
 }
