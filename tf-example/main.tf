ec946fdcf32b604af5abae145e2dcee490f3bed4
ab-abel <ab.bell205@gmail.com>
2024-11-22 15:41:09 +0100

    added deployment workflow

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -23,10 +23,23 @@ resource "aws_instance" "default" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = var.key_name
+  ecs_associate_public_ip_address = "true"
   vpc_security_group_ids = ["sg-0049869989aecfc10","sg-02e089c40be1dbe5e"]
   subnet_id = "subnet-01f56420769487d2f"
-  tags = {
+  
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install docker.io -y",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo docker pull elated205/3mtt:latest",
+      "sudo docker run -d -p 80:3000 elated205/3mtt:latest"
+    ]
+  }
+
+    tags = {
     Name = var.ec2_name
   }
 }
-
