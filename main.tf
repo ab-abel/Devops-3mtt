cac0403b04cbce012638bb711b1046f3b7cac803
ab-abel <ab.bell205@gmail.com>
2024-11-22 01:06:54 +0100

    corrections on AWS credentials

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "default" {
-  ami           = "ami-012967cc5a8c9f891"
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   key_name      = var.key_name
   tags = {
