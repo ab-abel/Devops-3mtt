6c7ca3f66276a6336a7dd54596683fa5bea562a2
ab-abel <ab.bell205@gmail.com>
2024-11-22 09:17:15 +0100

    corrected resourse name

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "default" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = var.key_name
